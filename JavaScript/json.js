ed1d10044b3304be633196d21d829237d565e1e7
bangdonghyuk <49140497+bangdonghyuk@users.noreply.github.com>
2021-01-04 17:48:17 +0900

    JAVASCRIPT 공부
    JAVASCRIPT 공부

---
+++ JavaScript/json.js
@@ -0,0 +1,49 @@
+// JSON
+// JavaScript Object Notaion
+
+// 1. Object to JSON
+// stringfy(obj)
+
+let json = JSON.stringify(true);
+console.log(json);
+
+json = JSON.stringify(["apple", "banana"]);
+console.log(json);
+
+const rabbit = {
+  name: "tori",
+  color: "white",
+  size: null,
+  birthDate: new Date(),
+  jump: () => {
+    console.log(`${name} can jump!`);
+  },
+};
+
+json = JSON.stringify(rabbit);
+console.log(json);
+
+json = JSON.stringify(rabbit, ["name", "size"]);
+console.log(json);
+
+json = JSON.stringify(rabbit, (key, value) => {
+  console.log(`key: ${key} , value:${value}`);
+  return key == "name" ? "ellie" : value;
+});
+console.log(json);
+
+// 2. JSON to Object
+// parse(json)
+
+json = JSON.stringify(rabbit);
+console.log(json);
+const obj = JSON.parse(json, (key, value) => {
+  console.log(`key: ${key} , value:${value}`);
+  return key == "birthDate" ? new Date(value) : value;
+});
+console.log(obj);
+rabbit.jump();
+// obj.jump();
+
+console.log(rabbit.birthDate.getDate());
+console.log(obj.birthDate.getDate());
---
+++ JavaScript/main.js
@@ -0,0 +1,8 @@
+// Whole-script strict mode syntax
+// JavaScript is very flexible
+// flexible === dangerous
+// added ECMAScript 5
+
+// 바닐라 스크립트 할때 사용 'use strict'
+"use strict";
+console.log("Hello World!");
